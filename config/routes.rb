74dddd61b5e80e776287b114c30170147ed85d2c
joybow <juntwolead2@gmail.com>
2023-12-13 16:07:24 +0900

    add the dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  
+  devise_for :admins, :controllers => {
+    :sessions => 'admins/sessions'
+  }
+
+  devise_scope :admin do
+    get "dashboard" , :to => "dashboard#index"
+    get "dashboard/login", :to => "admins/sessions#new"
+    post "dashboard/login", :to => "admins/sessions#create"
+    delete "dashboard/logout", :to => "admins/sessions#destroy"
+  end
+
+  get "dashboard", :to => "dashboard#index"
   resources :comments
   
   resources :posts do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,34 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Post.create!(
-    title: 'コーヒーラブ',
-    content: '美味しいコーヒー大好き',
-    user_id: 1,
-    )
+Admin.create!(
+  name: "永田純一",
+  email: "twolead.almighty.junrock@ezweb.ne.jp",
+  password: "Nj99159498",
+  password_confirmation: "Nj99159498"
 
-User.create!(
-  id: '3',
-  name:'永田楓',                         
-  postal_code: '000-001',
-  address: '愛知県名古屋市昭和です',                     
-  phone: '000-0000-001',
-  is_roaster: 'maker',
-  local: '愛知県',
-  email: 'test1@test1.com',
-  password: '222222',
-  password_confirmation: '222222'
-)
-
-User.create!(
-  id: '4',
-  name:'永田一路',                         
-  postal_code: '000-000',
-  address: '愛知県名古屋市瑞穂区',                     
-  phone: '000-0000-002',
-  is_roaster: 'customer',
-  local: '愛知県',
-  email: 'test3@test3.com',
-  password: '333333',
-  password_confirmation: '333333'
 )
