d561d9c99e205714162ee7ade223e986589371bb
joybow <juntwolead2@gmail.com>
2024-01-23 11:23:25 +0900

    like_ajax ka?

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,20 @@
 class FavoritesController < ApplicationController
   before_action :set_user
+  
+
 
   def create
     @post_favorite = Favorite.new(user_id: @user.id, post_id: params[:post_id])
     @post_favorite.save
-    redirect_to post_path(params[:post_id])
+    # redirect_to post_path(params[:post_id])
   end
   
   def destroy
     @post_favorite = Favorite.find_by(user_id: @user.id, post_id: params[:post_id])
     @post_favorite.destroy
-    redirect_to post_path(params[:post_id])
+    # redirect_to post_path(params[:post_id])
   end
+
 
   private
   def set_user
