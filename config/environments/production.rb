f25098962083268252f8a7c56e552d6160079915
joybow <juntwolead2@gmail.com>
2024-04-24 08:52:15 +0900

    i can't solve this problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,22 +117,18 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  Rails.application.configure do
-    # default url
-    config.action_mailer.default_url_options = {
+  config.action_mailer.default_url_options = {
     protocol: 'https',
     host: ENV["COFFEECOLOR_ROOT_URL"]
     }
-    # mail setting
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :user_name => ENV["GMAIL_ADDRESS"],
-      :password => ENV["GMAIL_2FACTOR_PASSWORD"],
-      :authentication => :plain,
-      :enable_starttls_auto => true
-    }
-  end
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => ENV["GMAIL_ADDRESS"],
+    :password => ENV["GMAIL_2FACTOR_PASSWORD"],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
--- db/migrate/20240422002417_change_post_id_to_bigint_in_favorites.rb
+++ db/migrate/20240422002417_change_post_id_to_bigint_in_favorites.rb
@@ -1,5 +1,9 @@
 class ChangePostIdToBigintInFavorites < ActiveRecord::Migration[6.1]
-  def change
+  def up
     change_column :favorites, :post_id, :bigint
+  end
+
+  def down
+    change_column :favorites, :post_id, :integer
   end
 end
