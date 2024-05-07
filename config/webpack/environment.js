1a7ea86f5797f07b4993602e6ee2ce2abcb4715d
joybow <juntwolead2@gmail.com>
2024-05-07 15:12:16 +0900

    change the file about environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,33 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
-const bootstrap = require('bootstrap')
+
 
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery',
     Popper: ['popper.js', 'default']
   })
 )
+
+if (process.env.NODE_ENV === 'production') {
+  environment.loaders.delete('file-loader');
+  environment.plugins.delete('file-loader');
+  environment.config.merge({
+    plugins: [
+      new webpack.NormalModuleReplacementPlugin(/bootstrap/,(result) =>{
+        result.request = undefined
+      })
+    ]
+  })
+} else {
+  const bootstrapEntries = ['bootstrap']
+  environment.entries.forEach((entry, name)=> {
+    bootstrapEntries.forEach((path)=>{
+      entry.import(path).catch(console.error)
+    })
+  })
+}
+
 module.exports = environment
