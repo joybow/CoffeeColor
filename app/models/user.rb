b003b68aea69579a0e9dc3a34bbdf3e0d9acfb57
joybow <juntwolead2@gmail.com>
2024-05-13 09:38:54 +0900

    change mistake in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ApplicationRecord
       :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, dependent: :destroy
   
-  has_many :faovirtes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_many :posts_favorites, through: :favorites,  source: :post, dependent: :destroy
 
   has_one_attached :user_image, service: :amazon
