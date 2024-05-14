46f33dd5796aa9cd66482729b0befd4bb4644132
joybow <juntwolead2@gmail.com>
2024-05-14 14:06:14 +0900

    why is this import 0

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,8 @@ import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "../channels";
-
+import $ from 'jquery';
+import 'bootstrap';
 import '../stylesheets/application';
 import '../javascripts/codeAddress.js';
 import '../javascripts/_form.js';
