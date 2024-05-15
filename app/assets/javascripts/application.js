7f9a5f2795c0c5ebebfe0f4acfb5d8ab8a7f2ad9
joybow <juntwolead2@gmail.com>
2024-05-15 20:33:41 +0900

    change yarn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,18 @@
-import $ from 'jquery';
-import '../stylesheets/application';
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require bootstrap
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
 
-const colorpicker = document.getElementById('arrow-icon-color-selector');
-  const arrowicon = document.getElementById('arrow-icon');
-  // カラーピッカーの入力値(inputの値)が変更されたら{}内の処理を実行する
-  colorpicker.addEventListener('input', function(e) {  
-    arrowicon.style.color = e.target.value; // アイコンのCSSのcolorプロパティの値を右辺の値にする。右辺の値とはすなわち、カラーピッカーでの選択値である。
-  });
+
+
+// const colorpicker = document.getElementById('arrow-icon-color-selector');
+//   const arrowicon = document.getElementById('arrow-icon');
+//   // カラーピッカーの入力値(inputの値)が変更されたら{}内の処理を実行する
+//   colorpicker.addEventListener('input', function(e) {  
+//     arrowicon.style.color = e.target.value; // アイコンのCSSのcolorプロパティの値を右辺の値にする。右辺の値とはすなわち、カラーピッカーでの選択値である。
+//   });
   
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "../channels";
-
+import 'jquery';
 import '../stylesheets/application';
 import '../javascripts/codeAddress.js';
 import '../javascripts/_form.js';
